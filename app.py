9b5e49a581e4d375555700ffadd1459364d10152
Matt Schreiber <schreibah@gmail.com>
2014-04-22 01:43:50 +0000

    Cleaning up testing

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from bottle import route, run
+import bottle
+import sqlite3
+
+app = application = bottle.Bottle()
 
 
-@route('/hello')
-def hello():
+@app.route('/login')
+def login():
+    return ""
+
+@app.route('/')
+def index():
     return "<h1>Hello, world!</h1>"
+
+
+if __name__ == "__main__":
+    bottle.run(host='localhost', port=9090)
