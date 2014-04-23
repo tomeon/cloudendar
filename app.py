5b11a8da24a88af3859789db4f09aacdbdd3ce26
Matt Schreiber <EMAIL>
2014-04-22 21:58:21 -0400

    Cleaning up

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import bottle
 import sqlite3
 
+# Enable debugging
+bottle.debug(True)
+
+# Automatically reload testing server
+bottle.run(reloader=True)
+
 app = application = bottle.Bottle()
 
 
@@ -8,10 +14,11 @@ app = application = bottle.Bottle()
 def login():
     return ""
 
+
 @app.route('/')
 def index():
     return "<h1>Hello, world!</h1>"
 
 
 if __name__ == "__main__":
-    bottle.run(host='localhost', port=9090)
+    bottle.run(app, host='localhost', port=8080)
