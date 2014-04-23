f4f7b75e3cacb2ea19b35002370567e64f6d682b
Matt Schreiber <EMAIL>
2014-04-23 16:40:28 -0400

    Cleaning up after I did "rm" instead of "git rm"...

--- webui.py
+++ webui.py
@@ -1,9 +1,13 @@
+<<<<<<< HEAD:webui.py
 import localpath
 from modules import bottle
 
+=======
+from bottle import route, run
+>>>>>>> parent of 9b5e49a... Cleaning up testing:app.py
 
-app = application = bottle.Bottle()
 
+<<<<<<< HEAD:webui.py
 
 @app.route('/login')
 def login():
@@ -17,3 +21,8 @@ def index():
 
 if __name__ == "__main__":
     bottle.run(app, host='localhost', port=8080, reloader=True, debug=True)
+=======
+@route('/hello')
+def hello():
+    return "<h1>Hello, world!</h1>"
+>>>>>>> parent of 9b5e49a... Cleaning up testing:app.py
