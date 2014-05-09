86c8e1cca14ca5d49abd1c0a392d5f9191ef94ff
Matt Schreiber <EMAIL>
2014-05-09 14:41:09 +0000

    Test websocket functionality

--- src/webapp.py
+++ src/webapp.py
@@ -143,21 +143,54 @@ def test_socketio():
 
 @socketio.on('my event', namespace='/test')
 def test_message(message):
-    emit('my response', {'data': message['data']})
+    flask.session['receive_count'] = flask.session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': flask.session['receive_count']})
 
 
 @socketio.on('my broadcast event', namespace='/test')
-def test_broadcast(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
+def test_message(message):
+    flask.session['receive_count'] = flask.session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': flask.session['receive_count']},
+         broadcast=True)
+
+
+@socketio.on('join', namespace='/test')
+def join(message):
+    join_room(message['room'])
+    flask.session['receive_count'] = flask.session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'In rooms: ' + ', '.join(request.namespace.rooms),
+          'count': flask.session['receive_count']})
+
+
+@socketio.on('leave', namespace='/test')
+def leave(message):
+    leave_room(message['room'])
+    flask.session['receive_count'] = flask.session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'In rooms: ' + ', '.join(request.namespace.rooms),
+          'count': flask.session['receive_count']})
+
+
+@socketio.on('my room event', namespace='/test')
+def send_room_message(message):
+    flask.session['receive_count'] = flask.session.get('receive_count', 0) + 1
+    emit('my response',
+        {'data': message['data'], 'count': flask.session['receive_count']},
+        room=message['room'])
 
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    emit('my response', {'data': 'Connected'})
+    emit('my response', {'data': 'Connected', 'count': 0})
+
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
