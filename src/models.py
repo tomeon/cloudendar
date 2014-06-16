86cdad65cf3a7144a77e6a95800a652f724c07d3
Matt Schreiber <EMAIL>
2014-06-16 12:32:07 -0400

    Major changes to scraper

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import (
     Boolean,
     Column,
+    Date,
     DateTime,
     ForeignKey,
     Integer,
@@ -8,9 +9,14 @@ from sqlalchemy import (
     PickleType,
     String,
     Table,
+    Time,
+    types,
 )
-from sqlalchemy.orm import backref, relationship
 from database import Base
+from datetime import datetime
+from dateutil.relativedelta import *
+from dateutil.rrule import *
+from sqlalchemy.orm import backref, relationship
 
 
 user_event = Table(
@@ -29,6 +35,19 @@ user_group = Table(
 )
 
 
+class WeekdayList(types.TypeDecorator):
+    """ Class for storing a list of dateutil.relativedelta 'weekday' types """
+    impl = types.PickleType
+
+    def convert_bind_param(self, weekdays, engine):
+        return self.impl.convert_bind_param([(weekday.weekday, weekday.n) for
+                                             weekday in weekday], engine)
+
+    def convert_result_value(self, value, engine):
+        weekdays = self.impl.convert_result_value(value, engine)
+        return [weekday(weekday.weekday, weekday.n) for weekday in weekdays]
+
+
 class User(Base):
     __tablename__ = 'user'
     onid = Column(String, primary_key=True, unique=True, index=True)
@@ -37,6 +56,7 @@ class User(Base):
     lname = Column(String(40), nullable=False)
     dept = Column(String(40), index=True)
     email = Column(String(40))
+    phone = Column(Integer())
     credentials = Column(PickleType())
     events = relationship("Event",
                           secondary=user_event,
@@ -48,13 +68,14 @@ class User(Base):
                           lazy='dynamic')
 
     def __init__(self, onid=None, fname=None, mname=None, lname=None, dept=None,
-                 email=None, credentials=None):
+                 email=None, phone=None, credentials=None):
         self.onid = onid
         self.fname = fname
         self.mname = mname
         self.lname = lname
         self.dept = dept
         self.email = email
+        self.phone = phone
         self.credentials = credentials
 
     def __repr__(self):
@@ -72,29 +93,59 @@ class User(Base):
     def get_id(self):
         return unicode(self.onid)
 
+    #def is_free(self, start_dt, end_dt):
+    #    events = Event.query.filter(Event.user.any(onid=self.onid)
+    #                                .filter()
+
 
 class Event(Base):
     __tablename__ = 'event'
     id = Column(Integer, primary_key=True)
-    start_date = Column(DateTime, index=True)
-    end_date = Column(DateTime)
-    attrb_name = Column(String(40))
-    location = Column(String(40))
+    start_date = Column(Date, index=True)
+    end_date = Column(Date, index=True)
+    start_time = Column(Time)
+    end_time = Column(Time)
+    weekdays = Column(WeekdayList)
     description = Column(String(80))
-    allday = Column(Boolean, default=False)
     duration = Column(Interval)
 
-    def __init__(self, start_date=None, end_date=None, attrb_name=None,
-                 location=None, description=None, allday=False):
+    def __init__(self, start_date=None, end_date=None,
+                 start_time=None, end_time=None,
+                 weekdays=None,
+                 duration=None,
+                 description=None):
         self.start_date = start_date
         self.end_date = end_date
-        self.attrb_name = attrb_name
-        self.location = location
+        self.start_time = start_time
+        self.end_time = end_time
+        self.weekdays = weekdays
+        self.duration = duration
         self.description = description
-        self.allday = allday
 
     def __repr__(self):
         return '<Event %r - %r>' % (self.start_date, self.end_date)
+
+    def get_freebusy(self, dtstart=None, until=None):
+        own_dtstart = datetime.combine(self.start_time, self.start_time)
+        own_until = datetime.combine(self.end_time, self.end_time)
+
+        dtstart = dtstart or own_dtstart
+        until = until or own_until
+
+        recur = rrule(WEEKLY, dtstart=dtstart, until=until,
+                      byweekday=self.weekdays)
+
+        freebusy_list = []
+        for start in recur:
+            end = start + self.duration
+            if end > until:
+                end = until
+            freebusy_list.append({'start': start, 'end': end})
+
+        # Return the list of busy times in the form that Google returns a
+        # freebusy query calendar
+        return {'busy': freebusy_list}
+
 
 
 class Group(Base):
--- src/scraper.py
+++ src/scraper.py
@@ -9,29 +9,13 @@ import sqlite3
 import itertools
 import urllib
 import urlparse
+
 from bs4 import BeautifulSoup
-
-
-CATALOG_URL = 'http://catalog.oregonstate.edu'
-SEARCHER_URL = CATALOG_URL + '/SOCSearcher.aspx'
-
-# Not sure what the difference is bewteen 'abcder' and 'abcdeg', but the first
-# returns more results.
-CATALOG_QUERY = {
-    'chr': 'abcder',
-    #'chr': 'abcdeg',
-}
-
-COURSE_LINK_REGEX = "ctl00_ContentPlaceHolder1_gvResults"
-
-COURSE_QUERY = {
-    'Columns' : 'abcdefghijklmnopqrstuvwxyz',
-}
-
-
-# Helper to open urls
-class MyOpener(urllib.FancyURLopener):
-    version = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15'
+from database import db_session, db_init
+from datetime import datetime
+from dateutil import parser
+from dateutil.relativedelta import *
+from models import Event, User
 
 
 # Adapted from user Wilfred Hughes' answer at:
@@ -44,17 +28,19 @@ def set_query_params(url, query_dict=None, *args, **kwargs):
     old_query_dict = dict(query_params)
 
     args_dict = {}
-    if isinstance(query_dict, dict):
-        old_query_dict.update(query_dict)
-    elif isinstance(query_dict, tuple):
-        if len(args) > 0:
-            args.insert(query_dict, 0)
-            args_dict = dict(args)
+    if query_dict is not None:
+        if isinstance(query_dict, dict):
+            old_query_dict.update(query_dict)
+        elif isinstance(query_dict, tuple):
+            if len(args) > 0:
+                args.insert(query_dict, 0)
+                args_dict = dict(args)
+            else:
+                args = query_dict
+                args_dict = {args[0]:args[1]}
+            pass
         else:
-            args = query_dict
-            args_dict = {args[0]:args[1]}
-    else:
-        raise ValueError()
+            raise ValueError()
 
     old_query_dict.update(args_dict)
     old_query_dict.update(kwargs)
@@ -64,41 +50,282 @@ def set_query_params(url, query_dict=None, *args, **kwargs):
     return urlparse.urlunsplit((scheme, netloc, path, new_query_string, fragment))
 
 
-def get_all(url, name=None, attrs={}, recursive=True, text=None, limit=None, **kwargs):
-    myopener = MyOpener()
-    page = myopener.open(url)
+CATALOG_URL = 'http://catalog.oregonstate.edu'
+SEARCHER_URL = CATALOG_URL + '/SOCSearcher.aspx'
 
-    text = page.read()
+DIRECTORY_URL = 'http://directory.oregonstate.edu/'
+DIRECTORY_QUERY = {
+    'type': 'search',
+    'cn': '',
+    'osudepartment': '',
+    'affiliation': 'any',
+    'join': 'and',
+}
+
+DIRECTORY_URL_QS = set_query_params(DIRECTORY_URL, DIRECTORY_QUERY)
+
+# Not sure what the difference is bewteen 'abcder' and 'abcdeg', but the first
+# returns more results.
+CATALOG_QUERY = {
+    'chr': 'abcder',
+    #'chr': 'abcdeg',
+}
+
+COURSE_LINK_REGEX = "ctl00_ContentPlaceHolder1_gvResults"
+
+COURSE_QUERY = {
+    # All options
+    #'Columns' : 'abcdefghijklmnopqrstuvwxyz',
+    # StartDate, EndDate, Weeks, CRN, Sec, Instructor, Day/Time/Date
+    'Columns': 'bcdjk',
+}
+
+COURSE_OFFERING_TABLE_ID = "ctl00_ContentPlaceHolder1_SOCListUC1_gvOfferings"
+
+
+DAY_MAP = {
+    'M': MO,
+    'T': TU,
+    'W': WE,
+    'R': TH,
+    'F': FR,
+    'S': SA,
+    'U': SU, # Is this the right letter for Sunday?
+}
+
+
+# Helper to open urls
+class MyOpener(urllib.FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15'
+
+
+OPENER = MyOpener()
+
+
+def get_all(url, name=None, attrs={}, recursive=True, text=None, limit=None, **kwargs):
+    page = OPENER.open(url)
+
+    page_text = page.read()
     page.close()
 
-    soup = BeautifulSoup(text)
+    soup = BeautifulSoup(page_text)
 
     return soup.find_all(name, attrs, recursive, text, limit, **kwargs)
 
 
-def get_category_links(url):
-    print(url)
-    print(COURSE_LINK_REGEX)
-    #tags = get_all(url, 'a', id=re.compile(COURSE_LINK_REGEX))
-    regex = re.compile(COURSE_LINK_REGEX)
-    tags = get_all(url, 'a', id=regex)
-    print(tags)
+def get_category_links():
+    tags = get_all(set_query_params(SEARCHER_URL, CATALOG_QUERY), 'a',
+                   id=re.compile(COURSE_LINK_REGEX))
 
     for tag in tags:
         yield tag['href']
 
 
-#def get_course_mappings():
+def get_course_mappings(path):
+    url = set_query_params(CATALOG_URL + path, COURSE_QUERY)
+
+    page = OPENER.open(url)
+
+    page_text = page.read()
+    page.close()
+
+    soup = BeautifulSoup(page_text)
+
+    table = soup.find('table', id=COURSE_OFFERING_TABLE_ID)
+    table_headers = map(lambda th: th.text, table.find_all('th'))
+
+    dept = soup.find('a', href=re.compile('CollegeOverview')).text
+    course_img = soup.find('img', alt='Course')
+    course = course_img.text
+    print("course: {0}".format(course_img))
+
+    for row in table.find_all('tr'):
+        data = map(lambda td: td.text.strip(), row.find_all('td'))
+        #dept = soup.find('a', href=re.compile('CollegeOverview')).text
+        if data:
+            course_dict = (dict(zip(table_headers, data)))
+            course_dict['dept'] = dept
+            course_dict['course'] = course
+            yield course_dict
+
+
+def parse_courseinfo(course):
+    StartDate = course.get('StartDate')
+    EndDate = course.get('EndDate')
+    DayTimeDate = course.get('Day/Time/Date')
+    Weeks = course.get('Weeks')
+    Instructor = course.get('Instructor')
+    dept = course.get('dept')
+
+    # Convert to datetime
+    #start_date = parser.parse(StartDate).date()
+    #end_date = parser.parse(EndDate).date()
+
+    # Figure out what days of the week the class occurs
+    day_re = re.match('^(\w+)\s+(\d{4})-(\d{4}).*$', DayTimeDate)
+    if day_re:
+        days = list(day_re.group(1))
+        days = map(lambda day: DAY_MAP.get(day), days)
+        start_time = day_re.group(2)
+        end_time = day_re.group(3)
+    else:
+        days = None
+        start_time = None
+        end_time = None
+
+    names = re.match('^(\w+),\s+(\w+).*$', Instructor)
+    if names:
+        instructor = {
+            'fname': names.group(2),
+            'lname': names.group(1),
+        }
+    else:
+        instructor = None
+
+    return {
+        'instructor' : instructor,
+        'start_date': StartDate,
+        'end_date': EndDate,
+        'start_time': start_time,
+        'end_time': end_time,
+        'days': days,
+        'weeks': Weeks,
+        'dept': dept,
+        'course': course.get('course')
+    }
+
+
+def courseinfo_to_model(info):
+    course_start_date = parser.parse(info.get('start_date')).date()
+    course_end_date = parser.parse(info.get('end_date')).date()
+
+    start_time = info.get('start_time')
+    end_time = info.get('end_time')
+
+    weekdays = info.get('days')
+    start_weekday = weekdays[0]
+    end_weekday = weekdays[len(weekdays) - 1]
+
+    start_date = course_start_date + relativedelta(weekday=start_weekday(+1))
+    end_date = course_end_date + relativedelta(weekday=end_weekday(-1))
+
+    # We don't worry about extracting JUST the start time, since
+    start_time_dt = datetime.strptime(start_time, '%H%M')
+    end_time_dt = datetime.strptime(end_time, '%H%M')
+    duration = end_time_dt - start_time_dt
+
+    return Event(
+        start_date=start_date,
+        end_date=end_date,
+        start_time=start_time_dt.time(),
+        end_time=end_time_dt.time(),
+        weekdays=weekdays,
+        duration=duration,
+        description=info.get('course'),
+    )
+
+
+def build_directory_query(info):
+    instructor = info.get('instructor')
+    return set_query_params(
+        DIRECTORY_URL_QS,
+        cn="{0} {1}".format(instructor.get('fname'), instructor.get('lname')),
+        osudepartment=info.get('dept')
+    )
+
+
+def remove_br_tags(elem, delim):
+    text = ''
+
+
+def strip_strings(elem, delim):
+    text = ''
+    for string in elem.stripped_strings:
+        text += string + delim
+    return text
+
+
+def instructor_dict_to_model(idict):
+    names = re.match('^(\w+),\s+(\w+)\s*(\w*).*$', idict.get('Full Name'))
+
+    if names:
+        fname = names.group(2)
+        mname = names.group(3)
+        lname = names.group(1)
+    else:
+        fname = None
+        mname = None
+        lname = None
+
+    phone_raw = idict.get('Office Phone Number')
+    phone = int(filter(lambda c: c >= '0' and c <= '9', phone_raw))
+
+    return User(
+        onid=idict.get('ONID Username'),
+        fname=fname,
+        mname=mname,
+        lname=lname,
+        dept=idict.get('Department'),
+        email=idict.get('Email Address'),
+        phone=phone
+    )
+
+
+def get_instructor_info(url):
+    page = OPENER.open(url)
+
+    page_text = page.read()
+    page.close()
+
+    soup = BeautifulSoup(page_text)
+    record = soup.find('div', {'class': 'record'})
+
+    # DEBUGGING
+    if record is None:
+        print("NO RECORD/MULTIPLE RECORDS FOR URL {0}".format(url))
+        sys.exit(1)
+
+    idict = {}
+    titles = record.find_all('dt')
+    for title in titles:
+        value = title.find_next_sibling('dd')
+        text = strip_strings(value, '\n')
+
+        idict[title.text.strip()] = text.strip()
+
+    return idict
 
 
 def main():
-    links = get_category_links(set_query_params(SEARCHER_URL, CATALOG_QUERY))
+    db_init()
+
+    links = get_category_links()
     for link in links:
-        #parsed = urlparse.urlsplit(link)
-        #queries = urlparse.parse_qsl(parsed.query)
-        #print(queries)
-        new_link = CATALOG_URL + set_query_params(link, COURSE_QUERY)
-        print(new_link)
+        courses = get_course_mappings(link)
+
+        for course in courses:
+            courseinfo = parse_courseinfo(course)
+
+            dir_url = build_directory_query(courseinfo)
+            idict = get_instructor_info(dir_url)
+
+            instructor = User.query.filter(User.onid == idict.get('onid')).first()
+            print(instructor)
+            if instructor is None:
+                instructor = instructor_dict_to_model(idict)
+                db_session.add(instructor)
+
+            print(courseinfo)
+            event = courseinfo_to_model(courseinfo)
+            instructor.events.append(event)
+            db_session.commit()
+            break
+
+        #db_session.commit()
+        break
+
+    db_session.commit()
+    db_session.remove()
 
 
 if __name__ == '__main__':
